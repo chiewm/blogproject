c8f43a869b23761a6dce563f9693eef97ad05828
chiewm <zmhbest@sina.cn>
2018-03-18 01:16:24 +0800

    add

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,15 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -24,3 +30,5 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
