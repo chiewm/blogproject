bbdd1685a2f000d79f3f6d59076f4c335e139fd8
chiewm <zmhbest@sina.cn>
2018-03-20 16:04:41 +0800

    add

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from comments.forms import CommentForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -42,6 +42,14 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
 
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+
+
 
 class CategoryView(IndexView):
     def get_queryset(self):
@@ -55,3 +63,11 @@ class Archives(IndexView):
         month = self.kwargs.get('month')
         return super(Archives, self).get_queryset().filter(created_time__year=year,
                                                            created_time__month=month)
+
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'blog/detail.html'
+#     context_object_name = 'post'
+#
+#
+#     def get(self, request, *args, ** ):
