6e0d8ef2c9d018124a433949834bf62db8b47309
chiewm <zmhbest@sina.cn>
2018-03-20 12:56:35 +0800

    add

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'blog'  # 命名空间
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.detail, name='detail'),
-    path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
-    path('category/<int:pk>/', views.category, name='category')
+    path('archives/<int:year>/<int:month>/', views.Archives.as_view(), name='archives'),
+    path('category/<int:pk>/', views.CategoryView.as_view(), name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from comments.forms import CommentForm
+from django.views.generic import ListView
+
 # Create your views here.
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
-
-
+# def index(request):
+#     post_list = Post.objects.all()
+#     return render(request, 'blog/index.html', context={
+#         'post_list': post_list
+#     })
+#
+#
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.increase_views()
@@ -20,17 +22,36 @@ def detail(request, pk):
                'form': form,
                'comment_list': comment_list}
     return render(request, 'blog/detail.html', context=context)
+#
+#
+# def archives(request, year, month):
+#     post_list = Post.objects.filter(created_time__year=year,
+#                                     created_time__month=month,
+#                                     )
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+#
+#
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate)
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month,
-                                    )
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
+
+class Archives(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(Archives, self).get_queryset().filter(created_time__year=year,
+                                                           created_time__month=month)
